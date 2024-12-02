c3ae632d0278f51d46ce4830bdaef3480ef6528e
Peetu Säkkinen <156309853+PeetuSak@users.noreply.github.com>
2024-12-02 10:50:31 +0200

    nordic koodi valmis

--- src/main.c
+++ src/main.c
@@ -1,6 +1,221 @@
 #include <zephyr/kernel.h>
 
+/*
+ * Copyright (c) 2023 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/gap.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/bluetooth/conn.h>
+#include <dk_buttons_and_leds.h>
+#include "my_lbs.h"
+
+#include "adc.h"
+
+uint32_t positio = 0;
+
+static struct bt_le_adv_param *adv_param = BT_LE_ADV_PARAM(
+	(BT_LE_ADV_OPT_CONNECTABLE |
+	 BT_LE_ADV_OPT_USE_IDENTITY), /* Connectable advertising and use identity address */
+	800, /* Min Advertising Interval 500ms (800*0.625ms) */
+	801, /* Max Advertising Interval 500.625ms (801*0.625ms) */
+	NULL); /* Set to NULL for undirected advertising */
+
+LOG_MODULE_REGISTER(Lesson4_Exercise2, LOG_LEVEL_INF);
+
+#define DEVICE_NAME CONFIG_BT_DEVICE_NAME
+#define DEVICE_NAME_LEN (sizeof(DEVICE_NAME) - 1)
+
+#define RUN_STATUS_LED DK_LED1
+#define CON_STATUS_LED DK_LED2
+#define USER_LED DK_LED3
+#define USER_BUTTON DK_BTN1_MSK
+
+#define STACKSIZE 1024
+#define PRIORITY 7
+
+#define RUN_LED_BLINK_INTERVAL 1000
+/* STEP 17 - Define the interval at which you want to send data at */
+#define NOTIFY_INTERVAL 500
+static bool app_button_state;
+/* STEP 15 - Define the data you want to stream over Bluetooth LE */
+static uint32_t app_sensor_value = 100;
+
+static const struct bt_data ad[] = {
+	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
+};
+
+static const struct bt_data sd[] = {
+	BT_DATA_BYTES(BT_DATA_UUID128_ALL, BT_UUID_LBS_VAL),
+};
+
+/* STEP 16 - Define a function to simulate the data */
+static void simulate_data(void)
+{
+	app_sensor_value++;
+	if (app_sensor_value == 200) {
+		app_sensor_value = 100;
+	}
+}
+static void app_led_cb(bool led_state)
+{
+	dk_set_led(USER_LED, led_state);
+}
+
+static bool app_button_cb(void)
+{
+	return app_button_state;
+}
+
+/* STEP 18.1 - Define the thread function  */
+void send_data_thread(void)
+{
+	int err;
+	err = dk_leds_init();
+	if (err) {
+		LOG_ERR("LEDs init failed (err %d)\n", err);
+		return;
+	}
+	
+	if(initializeADC() != 0)
+	{
+		printk("ADC initialization failed!");
+		return;
+	}
+
+    while (1) {
+		   struct Measurement m = readADCValue();
+
+        // Pakataan data taulukkoon
+        uint32_t sensor_value[4] = {m.x, m.y, m.z, positio};
+
+        // Lähetetään data taulukon sisältö kerralla
+        my_lbs_send_sensor_notify((uint8_t *)sensor_value, sizeof(sensor_value));
+
+        printk("Sent: x=%d, y=%d, z=%d, position=%d\n", m.x, m.y, m.z, positio);
+
+        k_sleep(K_MSEC(500));
+	}
+}
+
+static struct my_lbs_cb app_callbacks = {
+	.led_cb = app_led_cb,
+	.button_cb = app_button_cb,
+};
+
+static void button_changed(uint32_t button_state, uint32_t has_changed)
+{
+	if (has_changed & USER_BUTTON) {
+		uint32_t user_button_state = button_state & USER_BUTTON;
+
+		// Tarkista, onko painiketta painettu
+		if (user_button_state) {
+			// Lisää laskurin arvoa
+			positio++;
+
+			// Jos arvo menee yli 5, nollaa se
+			if (positio > 5) {
+				positio = 0;
+			}
+
+			// Tulostetaan positio muuttuja
+			printk("Position: %d\n", positio);
+
+			// Lähetä uusi tila indikointina
+			my_lbs_send_button_state_indicate(user_button_state);
+			app_button_state = true;
+		} else {
+			app_button_state = false;
+		}
+	}
+}
+
+static void on_connected(struct bt_conn *conn, uint8_t err)
+{
+	if (err) {
+		printk("Connection failed (err %u)\n", err);
+		return;
+	}
+
+	printk("Connected\n");
+
+	dk_set_led_on(CON_STATUS_LED);
+}
+
+static void on_disconnected(struct bt_conn *conn, uint8_t reason)
+{
+	printk("Disconnected (reason %u)\n", reason);
+
+	dk_set_led_off(CON_STATUS_LED);
+}
+
+struct bt_conn_cb connection_callbacks = {
+	.connected = on_connected,
+	.disconnected = on_disconnected,
+};
+
+static int init_button(void)
+{
+	int err;
+
+	err = dk_buttons_init(button_changed);
+	if (err) {
+		printk("Cannot init buttons (err: %d)\n", err);
+	}
+
+	return err;
+}
+
 int main(void)
 {
-        return 0;
+	int blink_status = 0;
+	int err;
+
+	LOG_INF("Starting Lesson 4 - Exercise 2 \n");
+
+	err = dk_leds_init();
+	if (err) {
+		LOG_ERR("LEDs init failed (err %d)\n", err);
+		return -1;
+	}
+
+	err = init_button();
+	if (err) {
+		printk("Button init failed (err %d)\n", err);
+		return -1;
+	}
+
+	err = bt_enable(NULL);
+	if (err) {
+		LOG_ERR("Bluetooth init failed (err %d)\n", err);
+		return -1;
+	}
+	bt_conn_cb_register(&connection_callbacks);
+
+	err = my_lbs_init(&app_callbacks);
+	if (err) {
+		printk("Failed to init LBS (err:%d)\n", err);
+		return -1;
+	}
+	LOG_INF("Bluetooth initialized\n");
+	err = bt_le_adv_start(adv_param, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
+	if (err) {
+		LOG_ERR("Advertising failed to start (err %d)\n", err);
+		return -1;
+	}
+
+	LOG_INF("Advertising successfully started\n");
+	for (;;) {
+		dk_set_led(RUN_STATUS_LED, (++blink_status) % 2);
+		k_sleep(K_MSEC(RUN_LED_BLINK_INTERVAL));
+	}
 }
+
+/* STEP 18.2 - Define and initialize a thread to send data periodically */
+K_THREAD_DEFINE(send_data_thread_id, STACKSIZE, send_data_thread, NULL, NULL, NULL, PRIORITY, 0, 0);
